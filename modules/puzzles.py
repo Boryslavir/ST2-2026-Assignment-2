05c620f84c8193c813d1ebec005b8f731abf89ca
Bing Tran <u3295557@uni.canberra.edu.au>
2026-05-16 12:37:41 +1000

    feat: change repo structure to be a bit more modular

---
+++ modules/puzzles.py
@@ -0,0 +1,4 @@
+def puzzles_module():
+    """Pathfinding, event simulation, DP puzzles"""
+    # TODO: Implement puzzle games
+    pass
---
+++ modules/sorting.py
@@ -0,0 +1,4 @@
+def sorting_module():
+    """Bubble sort, selection sort, merge sort visualizations"""
+    # TODO: Implement sorting visualizations
+    pass
