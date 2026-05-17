bfd8fa551392c6a070f28aa0fa54549b3dc7c905
Minh Duy Cao <caobill2005@gmail.com>
2026-05-17 16:47:32 +1000

    change of test module

--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -1,11 +1,16 @@
-import pytest
+import unittest
 
+
+# -----------------------------
+# Data Structure Logic Under Test
+# -----------------------------
 
 class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
+
 
 def insert(root, value):
     if root is None:
@@ -16,15 +21,18 @@ def insert(root, value):
         root.right = insert(root.right, value)
     return root
 
+
 def inorder(node):
-    if not node: 
+    if not node:
         return ""
     return inorder(node.left) + f"{node.value} " + inorder(node.right)
+
 
 def preorder(node):
     if not node:
         return ""
     return f"{node.value} " + preorder(node.left) + preorder(node.right)
+
 
 def postorder(node):
     if not node:
@@ -32,62 +40,81 @@ def postorder(node):
     return postorder(node.left) + postorder(node.right) + f"{node.value} "
 
 
+# -----------------------------
+# Unit Tests
+# -----------------------------
 
-def test_bst_inorder():
-    root = None
-    for v in [5, 3, 7, 2, 4, 6, 8]:
-        root = insert(root, v)
-    assert inorder(root) == "2 3 4 5 6 7 8 "
+class TestBST(unittest.TestCase):
 
-def test_bst_preorder():
-    root = None
-    for v in [5, 3, 7]:
-        root = insert(root, v)
-    assert preorder(root) == "5 3 7 "
+    def setUp(self):
+        """Build a small BST for reuse."""
+        self.values = [5, 3, 7, 2, 4, 6, 8]
+        self.root = None
+        for v in self.values:
+            self.root = insert(self.root, v)
 
-def test_bst_postorder():
-    root = None
-    for v in [5, 3, 7]:
-        root = insert(root, v)
-    assert postorder(root) == "3 7 5 "
+    def test_inorder(self):
+        self.assertEqual(inorder(self.root), "2 3 4 5 6 7 8 ")
 
+    def test_preorder(self):
+        root = None
+        for v in [5, 3, 7]:
+            root = insert(root, v)
+        self.assertEqual(preorder(root), "5 3 7 ")
 
-def test_linked_list_insert():
-    ll = []
-    ll.append(10)
-    ll.append(20)
-    assert ll == [10, 20]
-
-def test_linked_list_delete():
-    ll = [10, 20, 30]
-    ll.remove(20)
-    assert ll == [10, 30]
-
-def test_linked_list_reverse():
-    ll = [1, 2, 3]
-    ll.reverse()
-    assert ll == [3, 2, 1]
+    def test_postorder(self):
+        root = None
+        for v in [5, 3, 7]:
+            root = insert(root, v)
+        self.assertEqual(postorder(root), "3 7 5 ")
 
 
-def test_queue_enqueue():
-    q = []
-    q.append(5)
-    q.append(10)
-    assert q == [5, 10]
+class TestLinkedList(unittest.TestCase):
 
-def test_queue_dequeue():
-    q = [5, 10, 15]
-    q.pop(0)
-    assert q == [10, 15]
+    def test_insert(self):
+        ll = []
+        ll.append(10)
+        ll.append(20)
+        self.assertListEqual(ll, [10, 20])
+
+    def test_delete(self):
+        ll = [10, 20, 30]
+        ll.remove(20)
+        self.assertListEqual(ll, [10, 30])
+
+    def test_reverse(self):
+        ll = [1, 2, 3]
+        ll.reverse()
+        self.assertListEqual(ll, [3, 2, 1])
 
 
-def test_stack_push():
-    s = []
-    s.append(7)
-    s.append(9)
-    assert s == [7, 9]
+class TestQueue(unittest.TestCase):
 
-def test_stack_pop():
-    s = [1, 2, 3]
-    s.pop()
-    assert s == [1, 2]
+    def test_enqueue(self):
+        q = []
+        q.append(5)
+        q.append(10)
+        self.assertListEqual(q, [5, 10])
+
+    def test_dequeue(self):
+        q = [5, 10, 15]
+        q.pop(0)
+        self.assertListEqual(q, [10, 15])
+
+
+class TestStack(unittest.TestCase):
+
+    def test_push(self):
+        s = []
+        s.append(7)
+        s.append(9)
+        self.assertListEqual(s, [7, 9])
+
+    def test_pop(self):
+        s = [1, 2, 3]
+        s.pop()
+        self.assertListEqual(s, [1, 2])
+
+
+if __name__ == "__main__":
+    unittest.main()
